8d2d9416b04589dacaa594a106d8d7695ee5b9dc
mawedi <mohamedawedi10@gmail.com>
2023-11-30 20:58:09 +0100

    UPDATE

--- matrix/settings.py
+++ matrix/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-x)))fu5ejno1+35q6qj0#js@z=hyrbp8-aqv33yi8($upv4joy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 
 # Application definition
 INSTALLED_APPS = [
